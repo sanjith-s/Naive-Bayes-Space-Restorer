91d78a691120163f53176c7a7362c719b680b446
Laurence Dyer <ljdyer@gmail.com>
2022-11-17 07:44:52 +0000

    Update test_nb_space_restorer.py

--- tests/test_nb_space_restorer.py
+++ tests/test_nb_space_restorer.py
@@ -1,17 +1,53 @@
+"""test_nb_space_restorer.py
+
+Smoke tests to check that operations carried out in interactive demo run
+on sample data without errors."""
+
 from nb_space_restorer.nb_space_restorer import NBSpaceRestorer
 import pandas as pd
+from pytest import fixture
 
 train = pd.read_csv('sample_data/train.csv')['reference'].to_list()
-test = pd.read_csv('sample_data/test.csv')['input'].to_list()
-
-"""TODO: Test save functionality - are files created, and is model instance the same after loading? Same for grid search."""
+test_input = pd.read_csv('sample_data/test.csv')['input'].to_list()
+test_ref = pd.read_csv('sample_data/test.csv')['input'].to_list()
 
 
 # ====================
-def test_one():
+@fixture(scope='module')
+def restorer():
 
-    my_nb = NBSpaceRestorer(
+    restorer_ = NBSpaceRestorer(
         train,
         ignore_case=True
     )
-    print(my_nb.restore(test[0]))
+    yield restorer_
+
+
+# ====================
+def test_init(restorer):
+
+    restorer
+
+
+# ====================
+def test_grid_search(restorer):
+
+    restorer.add_grid_search(
+        'grid_search_1',
+        L=[18],
+        lambda_=[14.0],
+        ref=test_ref,
+        input=test_input
+    )
+
+
+# ====================
+def test_load_grid_search(restorer):
+
+    restorer.load_grid_search('grid_search_1')
+
+
+# ====================
+def test_show_optimal_params(restorer):
+
+    restorer.show_optimal_params()
